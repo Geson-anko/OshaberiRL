56f59f4a5ed6f25231118215cabf75880b88d6b0
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2021-12-31 21:19:07 +0900

    added Critic and Actor model

--- agent_models.py
+++ agent_models.py
@@ -4,6 +4,7 @@ from utils import AttrDict,get_stft_outch,get_stft_outlen,init_weights,get_paddi
 from torch.nn.utils import weight_norm,remove_weight_norm
 from torchsummaryX import summary
 from layers import ResBlock
+import math
 
 class BaseModel(nn.Module):
     """
@@ -75,6 +76,97 @@ class BaseModel(nn.Module):
             remove_weight_norm(l)
         for l in self.res_blocks:
             l.remove_weight_norm()
+
+def calculate_log_pi(log_stds, noises, actions):
+    """ 確率論的な行動の確率密度を返す． """
+    # ガウス分布 `N(0, stds * I)` における `noises * stds` の確率密度の対数(= \log \pi(u|a))を計算する．
+    # (torch.distributions.Normalを使うと無駄な計算が生じるので，下記では直接計算しています．)
+    gaussian_log_probs = \
+        (-0.5 * noises.pow(2) - log_stds).sum(dim=-1, keepdim=True) - 0.5 * math.log(2 * math.pi) * log_stds.size(-1)
+
+    # tanh による確率密度の変化を修正する．
+    log_pis = gaussian_log_probs - torch.log(1 - actions.pow(2) + 1e-6).sum(dim=-1, keepdim=True)
+
+    return log_pis
+
+
+def reparameterize(means, log_stds):
+    """ Reparameterization Trickを用いて，確率論的な行動とその確率密度を返す． """
+    # 標準偏差．
+    stds = log_stds.exp()
+    # 標準ガウス分布から，ノイズをサンプリングする．
+    noises = torch.randn_like(means)
+    # Reparameterization Trickを用いて，N(means, stds)からのサンプルを計算する．
+    us = means + noises * stds
+    # tanh　を適用し，確率論的な行動を計算する．
+    actions = torch.tanh(us)
+
+    # 確率論的な行動の確率密度の対数を計算する．
+    log_pis = calculate_log_pi(log_stds, noises, actions)
+
+    return actions, log_pis
+    
+class SACActor(nn.Module):
+
+    def __init__(self,h:AttrDict):
+        super().__init__()
+        self.h = h
+        self.layer1 = BaseModel(h)
+        ich = h.base_out_ch
+        och = h.action_space_size
+        self.layer_mean = nn.Linear(ich,och)
+        self.layer_log_std = nn.Linear(ich,och)
+
+    def forward(self, states:tuple[torch.Tensor]) -> torch.Tensor:
+        """
+        states : (src_spects, generated_spects)
+        return : (actions)
+        """
+        #assert states[0].shape == states[1].shape
+        x = self.layer1(states[0],states[1]).relu()
+        x = self.layer_mean(x).tanh()
+        return x
+
+    def sample(self,states:tuple[torch.Tensor]) -> torch.Tensor:
+        x = self.layer1(states[0],states[1])
+        means,log_stds = self.layer_mean(x),self.layer_log_std(x)
+        return reparameterize(means,log_stds.clamp(-20,2))
+
+class _sac_critic(nn.Module):
+
+    def __init__(self,h:AttrDict):
+        super().__init__()
+
+        self.s_ch = h.base_out_ch
+        self.a_ch = h.critic_action_hdim
+        self.outch = self.s_ch + self.a_ch
+
+        self.s_net = BaseModel(h)
+        self.a_net = nn.Sequential(
+            nn.Linear(h.action_space_size,self.a_ch),nn.ReLU(),
+            nn.Linear(self.a_ch,self.a_ch)
+        )
+        self.linear_post = nn.Linear(self.outch,1)
+
+    def forward(self, states:tuple[torch.Tensor],action:torch.Tensor) -> torch.Tensor:
+
+        x1 = self.s_net(states[0],states[1])
+        x2= self.a_net(action)
+        x = torch.cat([x1,x2],dim=-1).relu()
+        x = self.linear_post(x)
+        return x
+
+class SACCritic(nn.Module):
+    def __init__(self,h:AttrDict) -> None:
+        super().__init__()
+
+        self.net1 = _sac_critic(h)
+        self.net2 = _sac_critic(h)
+
+    def forward(self, states:tuple[torch.Tensor],actions:torch.Tensor) -> torch.Tensor:
+        return self.net1(states,actions),self.net2(states,actions)
+        
+
 
 
 
